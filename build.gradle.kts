86b088460846e16d668581ae27720f16240da9b9
trainb0y <trainb0ydev@gmail.com>
2023-05-09 11:28:15 -0600

    Use kts for gradle + cleanup

---
+++ build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+	java
+	kotlin("jvm")
+	id("fabric-loom")
+}
+
+group = property("maven_group")!!
+version = property("mod_version")!!
+
+repositories {
+	// Repositories for dependencies
+	mavenCentral()
+}
+
+dependencies {
+	// To change versions, see gradle.properties
+	minecraft("com.mojang:minecraft:${property("minecraft_version")}")
+	mappings("net.fabricmc:yarn:${property("yarn_mappings")}:v2")
+
+	modImplementation("net.fabricmc:fabric-loader:${property("loader_version")}")
+	modImplementation("net.fabricmc.fabric-api:fabric-api:${property("fabric_api_version")}")
+}
+
+tasks {
+	processResources {
+		inputs.property("version", project.version)
+		filesMatching("fabric.mod.json") {
+			expand(mutableMapOf("version" to project.version))
+		}
+	}
+
+	jar {
+		from("LICENSE")
+	}
+}
+
+java {
+	withSourcesJar()
+}
+
+kotlin {
+	jvmToolchain(17) // java version
+}
--- src/main/java/com/minecalc/MineCalc.java
+++ src/main/java/com/minecalc/MineCalc.java
@@ -1,4 +1,4 @@
-package net.fabricmc.minecalc;
+package com.minecalc;
 
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
