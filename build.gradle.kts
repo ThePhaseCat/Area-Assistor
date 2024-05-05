82efe5f780220b80c4f53375a09fb9d0fc08dfde
ThePhaseCat <alexmgamer10@gmail.com>
2024-05-05 13:50:46 -0600

    update to 1.20.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,8 +22,8 @@ dependencies {
 	modImplementation("net.fabricmc:fabric-loader:${property("loader_version")}")
 	modImplementation("net.fabricmc.fabric-api:fabric-api:${property("fabric_api_version")}+${property("minecraft_version")}")
 
-	modImplementation("io.github.cottonmc:LibGui:9.2.2+1.20.2")
-	include("io.github.cottonmc:LibGui:9.2.2+1.20.2")
+	modImplementation("io.github.cottonmc:LibGui:10.0.0+1.20.6")
+	include("io.github.cottonmc:LibGui:10.0.0+1.20.6")
 }
 
 tasks {
