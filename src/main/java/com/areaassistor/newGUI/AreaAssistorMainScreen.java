fd005df23712858f5098d2f05af932128c8a323b
ThePhaseCat <alexmgamer10@gmail.com>
2024-05-10 15:35:00 -0600

    block setting data ported to new ui system
    -it just works

--- src/main/java/com/areaassistor/newGUI/AreaAssistorMainScreen.java
+++ src/main/java/com/areaassistor/newGUI/AreaAssistorMainScreen.java
@@ -15,6 +15,10 @@ import org.jetbrains.annotations.NotNull;
 
 public class AreaAssistorMainScreen extends BaseOwoScreen<FlowLayout> {
 
+    public static BlockPos block1Pos = BlockPos.ORIGIN;
+    public static BlockPos block2Pos = BlockPos.ORIGIN;
+    public static BlockPos block3Pos = BlockPos.ORIGIN;
+
     @Override
     protected @NotNull OwoUIAdapter<FlowLayout> createAdapter() {
         return OwoUIAdapter.create(this, Containers::verticalFlow);
@@ -22,6 +26,10 @@ public class AreaAssistorMainScreen extends BaseOwoScreen<FlowLayout> {
 
     @Override
     protected void build(FlowLayout rootComponent) {
+        String block1Coords = "Block 1: " + blockCoords(block1Pos);
+        String block2Coords = "Block 2: " + blockCoords(block2Pos);
+        String block3Coords = "Block 3: " + blockCoords(block3Pos);
+
         rootComponent
                 .surface(Surface.VANILLA_TRANSLUCENT)
                 .horizontalAlignment(HorizontalAlignment.CENTER)
@@ -38,8 +46,13 @@ public class AreaAssistorMainScreen extends BaseOwoScreen<FlowLayout> {
         );
 
         rootComponent.child(
-                Containers.collapsible(Sizing.content(), Sizing.content(), Text.literal("Set Area"), false)
-                        .child(Components.button(Text.literal("Set Block 1 Position"), button -> { getBlockPos(); }))
+                Containers.collapsible(Sizing.content(), Sizing.content(), Text.literal("Set Area"), true)
+                        .child(Components.button(Text.literal("Set Block 1 Position"), button -> { setBlockPos(1); }))
+                        .child(Components.button(Text.literal("Set Block 2 Position"), button -> { setBlockPos(2); }))
+                        .child(Components.button(Text.literal("Set Block 3 Position"), button -> { setBlockPos(3); }))
+                        .child(Components.label(Text.literal(block1Coords)))
+                        .child(Components.label(Text.literal(block2Coords)))
+                        .child(Components.label(Text.literal(block3Coords)))
                         .padding(Insets.of(10)) //
                         .surface(Surface.DARK_PANEL)
                         .verticalAlignment(VerticalAlignment.CENTER)
@@ -47,6 +60,30 @@ public class AreaAssistorMainScreen extends BaseOwoScreen<FlowLayout> {
         );
     }
 
+
+    public void setBlockPos(int blockNum)
+    {
+        BlockPos blockPos = getBlockPos();
+        if(blockPos != null)
+        {
+            //AreaAssistorClient.LOGGER.info("Block " + blockNum + " Pos: " + blockPos);
+            switch(blockNum)
+            {
+                case 1:
+                    block1Pos = blockPos;
+                    MinecraftClient.getInstance().setScreen(new AreaAssistorMainScreen());
+                    break;
+                case 2:
+                    block2Pos = blockPos;
+                    MinecraftClient.getInstance().setScreen(new AreaAssistorMainScreen());
+                    break;
+                case 3:
+                    block3Pos = blockPos;
+                    MinecraftClient.getInstance().setScreen(new AreaAssistorMainScreen());
+                    break;
+            }
+        }
+    }
 
     //returns the block that the player is looking at
     public BlockPos getBlockPos()
@@ -59,12 +96,25 @@ public class AreaAssistorMainScreen extends BaseOwoScreen<FlowLayout> {
 
         if(hit.getType() == HitResult.Type.BLOCK)
         {
-            AreaAssistorClient.LOGGER.info("Block Pos: " + blockPos);
+            //AreaAssistorClient.LOGGER.info("Block Pos: " + blockPos);
             return blockPos;
         }
         else
         {
             return null;
+        }
+    }
+
+    public String blockCoords(BlockPos blockGiven)
+    {
+        if (blockGiven == null)
+        {
+            return "No Block Selected!";
+        }
+        else
+        {
+            String finalResult = blockGiven.getX() + ", " + blockGiven.getY() + ", " + blockGiven.getZ();
+            return finalResult;
         }
     }
 }
